6463e98fa06984aed1066875d161d97be95a9a20
tacotoemeck <wagner.tomek@gmail.com>
2020-03-12 12:01:56 +0000

    add js script comments Relates #1 Co-authored-by: hannahgooding <hannah.gooding@yahoo.com>

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // API URL's
 let movieDB_URL = "https://api.themoviedb.org/3/movie/";
+// hide an API key
 let movieDB_key = config.movieDB_key;
 
-// create a random number between 100-500
+// create a random number between 100-500 to limit database results ( arbitrarily )
 
 function generateRandomNumber() {
   return Math.floor(Math.random() * (500 - 100 + 1)) + 100;
 }
-
+// initialize empty movie array to push movie object to
 let movieArray = [];
+// initialize accumulator array
 let idArray = [];
 
 // generate 4 random movie objects and push the into an array
@@ -20,17 +22,21 @@ async function generateMovies() {
     let movieId = generateRandomNumber();
     // create new url on each iteration
     let requestURL = `${movieDB_URL}${movieId}${movieDB_key}`;
-
+    // use try to catch API call errors
     try {
       // fetch movie using generated url
       let response = await fetch(requestURL);
       let movie = await response.json();
-      // check if movie exists in the database ( checking response status code )
+
       if (
+        // check if movie exists in the database ( checking response status code )
         movie.status_code !== 34 &&
+        // make sure movie has a poster image
         movie.poster_path !== null &&
+        // make sure there are no duplicates in the accumulator array (idArray)
         !idArray.includes(movie.id)
       ) {
+        // create a movie object with specified keys
         idArray.push(movie.id);
         movieObj.id = movie.id;
         movieObj.img = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
@@ -44,10 +50,6 @@ async function generateMovies() {
   }
 
   console.log(movieArray);
-
-  // create new movie obj with with title, image and id
-
-  // if not in movie array, push it in
 }
 
 generateMovies();

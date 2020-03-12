36278b973e1a868cdb950a22692df932870d422a
tacotoemeck <wagner.tomek@gmail.com>
2020-03-12 16:25:34 +0000

    Add generateGif API call Relates #1 Co-authored-by: glrta <to.gio@pm.me>

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // API URL's
 let movieDB_URL = "https://api.themoviedb.org/3/movie/";
+let giphy_URL = "https://api.giphy.com/v1/gifs/search?api_key=";
+
+// lbwQZXxYMqPh2DpvV8nXbGlqHNm8kJ9h&q=basic-instinct&limit=1&offset=0&rating=G&lang=en
+
 // hide an API key
 let movieDB_key = config.movieDB_key;
-
+let giphy_key = config.giphy_key;
 // create a random number between 100-500 to limit database results ( arbitrarily )
 
-function generateRandomNumber() {
-  return Math.floor(Math.random() * (500 - 100 + 1)) + 100;
+function generateRandomNumber(max, min) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 // initialize empty movie array to push movie object to
 let movieArray = [];
@@ -19,7 +23,7 @@ async function generateMovies() {
   // fetch movies until array has 4 objects
   while (movieArray.length < 4) {
     let movieObj = {};
-    let movieId = generateRandomNumber();
+    let movieId = generateRandomNumber(500, 100);
     // create new url on each iteration
     let requestURL = `${movieDB_URL}${movieId}${movieDB_key}`;
     // use try to catch API call errors
@@ -41,6 +45,7 @@ async function generateMovies() {
         movieObj.id = movie.id;
         movieObj.img = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
         movieObj.title = movie.title;
+        // push movieObj into moviearray
         movieArray.push(movieObj);
       }
     } catch (err) {
@@ -48,8 +53,38 @@ async function generateMovies() {
       alert("Something's gone wrong, please try again");
     }
   }
-
   console.log(movieArray);
 }
+// choose random movie from the array and replace white spaces in the title with hyphen
+function moviePicker() {
+  let randomIndex = generateRandomNumber(3, 0);
+  let movieTitle = movieArray[randomIndex].title;
 
-generateMovies();
+  movieTitle = movieTitle.replace(/[.,\/#!$%\^&\*;:{}=\_`~(),\s ]/g, " ");
+  movieTitle = movieTitle.replace(/ /g, "-").toLowerCase();
+
+  // generate request URL
+  let requestURL = `${giphy_URL}${giphy_key}&q=${movieTitle}&limit=1&offset=0&rating=G&lang=en`;
+
+  return requestURL;
+}
+
+async function generateGif() {
+  // fetch gif with a specic title
+  let requestURL = moviePicker();
+
+  try {
+    // fetch movie using generated url
+    let response = await fetch(requestURL);
+    let gif = await response.json();
+    let gifURL = gif.data[0].images.downsized_large.url;
+  } catch (err) {
+    // catches errors both in fetch and response.json
+    alert("Something's gone wrong, please try again");
+  }
+
+  return gifURL;
+}
+
+// generateGif function needs to be called AFTER generateMovies
+generateMovies().then(generateGif);
